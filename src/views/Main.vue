a62655d506c8f07c7f87b0743523e08803def104
zhoufei <1832729975@qq.com>
2020-01-16 22:51:11 +0800

    init ok ~@zf[2020/01/16 22:51:11  周四]

---
+++ src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  components: {}
+})
+export default class Home extends Vue {}
+</script>
