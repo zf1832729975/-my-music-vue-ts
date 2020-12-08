3a28d3be26453184bf78078cc46588606f2802cf
zhoufei <1832729975@qq.com>
2020-12-08 19:04:55 +0800

    主题生成 ~@zf[2020/12/08 19:04:54  周二]

--- src/components/SiteHeader/generate-theme.js
+++ src/components/SiteHeader/generate-theme.js
@@ -1,13 +1,37 @@
-async function generateTheme(themeEl) {
-  let styleStr = ''
+// themeEl = document.getElementById('_theme_link')
+// generateTheme(themeEl, '#3396fc', '#fc5531')
+
+let cuStyle
+
+/**
+ * 生成主题
+ * @param {*} themeEl 主题颜色的 el
+ * @param {*} oldThemeColor 旧主题颜色
+ * @param {*} newThemColor 新主意颜色
+ */
+export async function generateTheme(themeEl, oldThemeColor, newThemColor) {
   if (themeEl.tagName === 'LINK') {
-    styleStr = await getCSSString(themeEl.href)
+    const styleStr = await getCSSString(themeEl.href)
+
+    if (!cuStyle) {
+      cuStyle = document.createElement('style')
+      cuStyle.setAttribute('id', 'cu-style--' + parseInt(Math.random() * 1000))
+
+      document.head.append(cuStyle)
+    }
+
+    cuStyle.innerText = styleStr.replace(
+      new RegExp(oldThemeColor, 'ig'),
+      newThemColor
+    )
   } else if (themeEl.tagName === 'STYLE') {
-    styleStr = themeEl.innerText
+    themeEl.innerText = themeEl.innerText.replace(
+      new RegExp(oldThemeColor, 'ig'),
+      newThemColor
+    )
   } else {
     throw new Error('themeEl 不符合条件！')
   }
-  console.log('styleStr : ', styleStr)
 }
 
 function getCSSString(url) {
